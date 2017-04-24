3dff38509fde159a539bb59d11be8b545448fc4f
Kyle Benson <kylewaynebenson@gmail.com>
2017-04-24 10:32:13 -0600

    style for trial fonts AND shopping cart

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -85,7 +85,47 @@ get_header( 'shop' ); ?>
 			<?php wc_get_template( 'loop/no-products-found.php' ); ?>
 
 		<?php endif; ?>
-
+		<?php if ( is_product_category('type') ) : ?>
+			<div class="clear"></div>
+			<article class="product product-trail-fonts type-product">
+				<div class="trial-fonts-text">
+					<h5>Free Trial Fonts</h5>
+					<p>Here at Very Cool, we know how important it is to handle the merchandise before you buy. That’s why we’re offering you a free trial download of each font with a newsletter subscription. After submitting the form, you’ll receive a subscription confirmation email. After accepting that, you'll receive a final email with a zip folder chock full of (<small>limited-capability</small>) trial fonts. Then you can experiment your heart out with typefaces like Cardinal Grotesque and Maritime Champion! Happy trials!</p>
+				</div>
+				<form action="//verycoolstudio.us4.list-manage.com/subscribe/post?u=edc2737100be661c0fc9e2c5e&amp;id=1e6678ac80" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="trial-fonts-fields validate" target="_blank" novalidate>
+				    <div>
+					<p class="form-row form-row validate-required">
+						<label for="mce-EMAIL">Email Address </label>
+						<input type="email" value="" name="EMAIL" class="input-text required email" id="mce-EMAIL">
+					</p>
+					<p class="form-row form-row form-row-first">
+						<label for="mce-FNAME">First Name </label>
+						<input type="text" value="" name="FNAME" class="input-text " id="mce-FNAME">
+					</p>
+					<p class="form-row form-row form-row-last">
+						<label for="mce-LNAME">Last Name </label>
+						<input type="text" value="" name="LNAME" class="input-text " id="mce-LNAME">
+					</p>
+					<p class="form-row terms terms-and-conditions">
+						<input type="checkbox" class="input-checkbox" name="terms" id="terms">
+						<label for="terms" class="checkbox">I accept the trail <a href="<?php echo esc_url( home_url( '/' ) ); ?>free-trials" target="_blank">terms &amp; conditions</a></label>
+						<input type="hidden" name="terms-field" value="1">
+					</p>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>
+				    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_edc2737100be661c0fc9e2c5e_1e6678ac80" tabindex="-1" value=""></div>
+				    <p class="clear">
+				    	<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button chamfered-button chamfered-button-yellow">
+				    </p>
+				    </div>
+				</form>
+				<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+				<script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+				<!--End mc_embed_signup-->
+			</article>
+		<?php endif; ?>
 	<?php
 		/**
 		 * woocommerce_after_main_content hook.
